34e195044e85813867b53d8620cac63b66f30337
ildar <ildar.dautov01@gmail.com>
2021-06-14 04:11:40 +0300

    fix(routing): Исправлена навигация

--- src/app/pages/modules/modules-routing.module.ts
+++ src/app/pages/modules/modules-routing.module.ts
@@ -7,30 +7,34 @@ const routes: Routes = [
   {
     path: '',
     component: ModulesPage,
-  },
-  {
-    path: 'search',
-    loadChildren: () =>
-      import('./pages/search/search.module').then(m => m.SearchPageModule),
-  },
-  {
-    path: 'analytics',
-    loadChildren: () =>
-      import('./pages/analytics/analytics.module').then(
-        m => m.AnalyticsPageModule
-      ),
-  },
-  {
-    path: 'messages',
-    loadChildren: () =>
-      import('./pages/messages/messages.module').then(
-        m => m.MessagesPageModule
-      ),
-  },
-  {
-    path: 'profile',
-    loadChildren: () =>
-      import('./pages/profile/profile.module').then(m => m.ProfilePageModule),
+    children: [
+      {
+        path: 'search',
+        loadChildren: () =>
+          import('./pages/search/search.module').then(m => m.SearchPageModule),
+      },
+      {
+        path: 'analytics',
+        loadChildren: () =>
+          import('./pages/analytics/analytics.module').then(
+            m => m.AnalyticsPageModule
+          ),
+      },
+      {
+        path: 'messages',
+        loadChildren: () =>
+          import('./pages/messages/messages.module').then(
+            m => m.MessagesPageModule
+          ),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('./pages/profile/profile.module').then(
+            m => m.ProfilePageModule
+          ),
+      },
+    ],
   },
 ];
 
