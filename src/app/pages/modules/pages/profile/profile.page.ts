ca2844b5535fa6705061aecf3fbeb0d183ea8a7c
ildar <ildar.dautov01@gmail.com>
2021-08-29 00:18:07 +0300

    fix: исправлены ошибки с роутингом и анимацией

--- src/app/pages/modules/pages/profile/profile.page.ts
+++ src/app/pages/modules/pages/profile/profile.page.ts
@@ -6,7 +6,5 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-    constructor() {}
-
     ngOnInit() {}
 }
