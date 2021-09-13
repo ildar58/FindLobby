2d0c27f26c22665beaecd26df981c38ad362fa36
ildar <ildar.dautov01@gmail.com>
2021-09-13 19:11:30 +0300

    feat: Сделано много всего

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-    production: true,
+    production: false,
+    firebaseConfig: {
+        apiKey: 'AIzaSyBgeNSvPpdb0NaPtullEI2fxonshqSwg_A',
+        authDomain: 'findlobby-a1e65.firebaseapp.com',
+        databaseURL:
+            'https://findlobby-a1e65-default-rtdb.europe-west1.firebasedatabase.app',
+        projectId: 'findlobby-a1e65',
+        storageBucket: 'findlobby-a1e65.appspot.com',
+        messagingSenderId: '856280690834',
+        appId: '1:856280690834:web:f6d7b352fa3ee93cd0764b',
+        measurementId: 'G-36XHNNWQJL',
+    },
 };
